1e2bea39333bedee6cf01431f9a4d8d989881c44
Matheus <matheuspichler94@gmail.com>
2025-05-11 23:52:18 -0300

    Interfaces

---
+++ src/Desafio_3/Ninja.java
@@ -0,0 +1,17 @@
+package Desafio_3;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String missao;
+    String nivelDificuldade;
+    String statusMissao;
+
+    public void mostrarInformacoes(){
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(missao);
+        System.out.println(nivelDificuldade);
+        System.out.println(statusMissao);
+    }
+}
